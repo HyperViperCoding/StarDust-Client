6d81294e1e527e9487da72b682744ebe40507928
devvv-3vs <63360726+devvv-3vs@users.noreply.github.com>
2020-06-05 10:41:39 -0400

    epic
    epic gamer with xboost yesfall zboost tabgui stupidscaffold and airwater which is just nowater for now

---
+++ YesFall.cs
@@ -0,0 +1,18 @@
+﻿using Flare_Sharp.ClientBase.Categories;
+using Flare_Sharp.Memory.FlameSDK;
+using System;
+
+namespace Flare_Sharp.ClientBase.Modules.Modules
+{
+    public class YesFall : Module
+    {
+        public YesFall() : base("YesFall", CategoryHandler.registry.categories[2], (char)0x07, false)
+        {
+        }
+        public override void onTick()
+        {
+            base.onTick();
+            Minecraft.clientInstance.localPlayer.isFalling = 1;
+        }
+    }
+}
