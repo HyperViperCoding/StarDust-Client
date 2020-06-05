292e42de2c5b6e0c91093c28c6f869f54325e787
devvv-3vs <63360726+devvv-3vs@users.noreply.github.com>
2020-06-05 14:41:10 -0400

    f8xed tfrehki

--- Flare Sharp/ClientBase/Modules/Modules/TFreaky.cs
+++ Flare Sharp/ClientBase/Modules/Modules/TFreaky.cs
@@ -1,4 +1,4 @@
-﻿using Flare_Sharp.ClientBase.Categories;
+using Flare_Sharp.ClientBase.Categories;
 using Flare_Sharp.Memory.FlameSDK;
 using System;
 using System.Collections.Generic;
@@ -9,8 +9,7 @@ namespace Flare_Sharp.ClientBase.Modules.Modules
     public class TFreaky : Module
     {
         public TFreaky() : base("TFreaky", CategoryHandler.registry.categories[0], (char)0x07, false)
- 
+        {
         }
-
-
-     
+    }
+}
